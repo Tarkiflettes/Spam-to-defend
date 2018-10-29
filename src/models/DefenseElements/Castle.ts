8c635d3240a423e3ac55849bdf61e922cd268416
Lionel Jouin <lionel.jouin@outlook.com>
2018-10-29 20:49:43 +0100

    Castle

--- src/models/DefenseElements/Castle.ts
+++ src/models/DefenseElements/Castle.ts
@@ -5,7 +5,7 @@ export class Castle extends Element {
 
     constructor() {
         super();
-        
+
         let stats = balancing.element.castle;
         this.setmaxHealth(stats.maxHealth, true);
     }
@@ -13,19 +13,61 @@ export class Castle extends Element {
     public destroy(): void {
         super.destroy();
     }
-    
+
     draw(): void {
+        let color = 0x1b2631;
+        let bodyColor = 0x2c3e50;
         let size = 100;
 
         var rect = new PIXI.Graphics();
-        rect.beginFill(0x2c3e50, 1);
+        rect.beginFill(bodyColor, 1);
         rect.drawRect(-size / 2, -size / 2, size, size);
+        this.addChild(rect);
+
+        var rect = new PIXI.Graphics(); // left top
+        rect.beginFill(color, 1);
+        rect.drawRect(-size / 2, - size / 2, size / 6, size / 6);
+        this.addChild(rect);
+
+        var rect = new PIXI.Graphics(); // left middle
+        rect.beginFill(color, 1);
+        rect.drawRect(-size / 2, 0 - (size / (6 * 2)), size / 6, size / 6);
+        this.addChild(rect);
+
+        var rect = new PIXI.Graphics(); // left bottom
+        rect.beginFill(color, 1);
+        rect.drawRect(-size / 2, size / 2 - (size / 6), size / 6, size / 6);
+        this.addChild(rect);
+
+        var rect = new PIXI.Graphics(); // right top
+        rect.beginFill(color, 1);
+        rect.drawRect(size / 2 - (size / 6), - size / 2, size / 6, size / 6);
+        this.addChild(rect);
+
+        var rect = new PIXI.Graphics(); // right middle
+        rect.beginFill(color, 1);
+        rect.drawRect(size / 2 - (size / 6), - size / (6 * 2), size / 6, size / 6);
+        this.addChild(rect);
+
+        var rect = new PIXI.Graphics(); // right bottom
+        rect.beginFill(color, 1);
+        rect.drawRect(size / 2 - (size / 6), size / 2 - (size / 6), size / 6, size / 6);
+        this.addChild(rect);
+
+        var rect = new PIXI.Graphics(); // middle top
+        rect.beginFill(color, 1);
+        rect.drawRect(- size / (6 * 2), - size / 2, size / 6, size / 6);
+        this.addChild(rect);
+
+        var rect = new PIXI.Graphics(); // middle bottom
+        rect.beginFill(color, 1);
+        rect.drawRect(- size / (6 * 2), size / 2 - (size / 6), size / 6, size / 6);
         this.addChild(rect);
     }
 
     public start(): void {
     }
-    
+
     public update(deltatime: number): void {
     }
 
